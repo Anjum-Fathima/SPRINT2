9cb8aea70fbfaf76b3423aa084f7260dd637bad3
Anjum-Fathima <anjumfathima9899@gmail.com>
2021-02-18 12:05:11 +0530

    updated

--- hsm/src/main/java/com/cg/hsm/domain/User.java
+++ hsm/src/main/java/com/cg/hsm/domain/User.java
@@ -16,68 +16,82 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-@Table(name="users")
-public class User 
-{
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long userId;
-   
-   @NotBlank(message = "userName is required")
-   @Column(updatable=false,unique=true)
-   private String userName;
-   
-   @NotBlank(message = "userPassword is required")
-   @Size(min = 8 ,max = 15 ,message = "password size should between 8 to 15 characters")
-   private String userPassword;
-   
-   @JsonFormat(pattern = "yyyy-MM-dd")
-   private Date created_On;
-   
-   @JsonFormat(pattern = "yyyy-MM-dd")
-   private Date updated_On;
-   
-   private String created_By="Admin";
-   
-public Long getUserId() {
-	return userId;
-}
-public void setUserId(Long userId) {
-	this.userId = userId;
-}
-public String getUserName() {
-	return userName;
-}
-public void setUserName(String userName) {
-	this.userName = userName;
-}
-public String getUserPassword() {
-	return userPassword;
-}
-public void setUserPassword(String password) {
-	this.userPassword = password;
-}
-public Date getCreated_On() {
-	return created_On;
-}
-public void setCreated_On(Date created_At) {
-	this.created_On = created_At;
-}
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+    
+	// userName of the user
+	@NotBlank(message = "userName is required")
+	@Column(updatable = false, unique = true)
+	private String userName;
 
-public String getCreated_By() {
-	return created_By;
-}
-public void setCreated_By(String created_By) {
-	this.created_By = created_By;
-}
-@PrePersist
-public void onCreate()
-{
-	this.created_On=new Date();
-}
-@PreUpdate
-public void onUpdate()
-{
-	this.updated_On=new Date();
-}
+	//userPassword of the user
+	@NotBlank(message = "userPassword is required")
+	@Size(min = 8, max = 15, message = "password size should between 8 to 15 characters")
+	private String userPassword;
+
+	//Date of user registration
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date created_On;
+
+	//Date of updation of user data
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date updated_On;
+
+	private String created_By = "Admin";
+    
+	// getters and setters
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String password) {
+		this.userPassword = password;
+	}
+
+	public Date getCreated_On() {
+		return created_On;
+	}
+
+	public void setCreated_On(Date created_At) {
+		this.created_On = created_At;
+	}
+
+	public String getCreated_By() {
+		return created_By;
+	}
+
+	public void setCreated_By(String created_By) {
+		this.created_By = created_By;
+	}
+    
+	//Automatic generation of created_On
+	@PrePersist
+	public void onCreate() {
+		this.created_On = new Date();
+	}
+
+	//Automatic generation of updated_On
+	@PreUpdate
+	public void onUpdate() {
+		this.updated_On = new Date();
+	}
 }
--- hsm/src/main/java/com/cg/hsm/service/UserService.java
+++ hsm/src/main/java/com/cg/hsm/service/UserService.java
@@ -8,48 +8,47 @@ import com.cg.hsm.exception.UserNameException;
 import com.cg.hsm.repository.UserRepository;
 
 @Service
-public class UserService
-{
-   @Autowired
-   private UserRepository userRepository;
-   
-   public User saveOrUpdate(User user)
-   {
-	   try 
-	   {
-		   user.setUserName(user.getUserName().toUpperCase());
-		   return userRepository.save(user);
-	   }
-	   catch(Exception e)
-	   {
-		 throw new UserNameException("User Name : "+user.getUserName().toUpperCase()+" already exists");  
-	   }
-	 
-   }
-   
-   
-   public User findByUserName(String userName)
-   {
-	   User user=userRepository.findByUserName(userName.toUpperCase());
-	   if(user==null)
-	   {
-		  throw new UserNameException("User Name : "+userName.toUpperCase()+" does not exists"); 
-	   }
-	   return user;
-   }
-   
-   public Iterable<User> getAllUsers()
-   {
-	   return userRepository.findAll();
-   }
-   
-   public void deleteByUserName(String userName)
-   {
-	   User user = userRepository.findByUserName(userName.toUpperCase());
-	   if(user==null)
-	   {
-		   throw new UserNameException("User Name : "+userName.toUpperCase()+" does not exists"); 
-	   }
-	   userRepository.delete(user);
-   }
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	// Adding user details into the table
+	public User saveUser(User user) {
+		try {
+			user.setUserName(user.getUserName().toUpperCase());
+			return userRepository.save(user);
+		} catch (Exception e) {
+			throw new UserNameException("User Name : " + user.getUserName().toUpperCase() + " already exists");
+		}
+
+	}
+
+	// Finding user details using userName
+	public User findByUserName(String userName) {
+		User user = userRepository.findByUserName(userName.toUpperCase());
+		if (user == null) {
+			throw new UserNameException("User Name : " + userName.toUpperCase() + " does not exists");
+		}
+		return user;
+	}
+
+	// Finding all users
+	public Iterable<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+	// Deleting a user based on userName
+	public void deleteByUserName(String userName) {
+		User user = userRepository.findByUserName(userName.toUpperCase());
+		if (user == null) {
+			throw new UserNameException("User Name : " + userName.toUpperCase() + " does not exists");
+		}
+		userRepository.delete(user);
+	}
+
+	// Updating user password
+	public void updateUserPassword(String userName, User user) {
+		userRepository.save(user);
+	}
+
 }
